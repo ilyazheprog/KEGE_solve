4805dcf7e2f02b58f83f1cb09a7fab9b5dbe1f1f
Marat2004 <107705653+SHagitov@users.noreply.github.com>
2022-06-17 14:17:12 -0400

    поправки

--- 2021 и 2022/Сайт К.Ю Полякова/15/отрезки/22.py
+++
@@ -1,5 +0,0 @@
-def f(x): return (x&25!=0) <= ((x&17==0) <= (x&a!=0))
-for a in range(1,1000):
-    if all(f(x)==1 for x in range(1,10000)):
-        print(a)
-        break
--- 2021 и 2022/Сайт К.Ю Полякова/15/отрезки/370.py
+++
@@ -1,7 +0,0 @@
-def f(x):
-    return (x%a!=0) <= ((x%6==0) <= (x%4!=0))
-ans = []
-for a in range(1,1000):
-    if all(f(x)==1 for x in range(1,10000)):
-        ans += [a]
-print(max(ans))
