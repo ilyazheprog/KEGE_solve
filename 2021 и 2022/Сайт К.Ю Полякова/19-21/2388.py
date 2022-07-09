5ab798d965871f4a65c06fb3acdc598af9138ddd
Marat2004 <testinger2021@yandex.ru>
2022-07-09 13:19:04 +0500

    Create 2388.py

---
+++ 2021 и 2022/Сайт К.Ю Полякова/19-21/2388.py
@@ -0,0 +1,12 @@
+def f(a, b, c, m):
+    if a + b >= 49: return c % 2 == m % 2
+    if c == m: return 0
+    h = [f(a + 1, b, c + 1, m), f(a, b + 1, c + 1, m), f(a * 3, b, c + 1, m), f(a, b * 3, c + 1, m)]
+    return any(h) if (c + 1) % 2 == m % 2 else all(h)
+
+
+for b in range(1, 43 + 1):
+    for m in range(1, 5):
+        if f(5, b, 0, m) == 1:
+            if m == 4: print(b, m)
+            break
