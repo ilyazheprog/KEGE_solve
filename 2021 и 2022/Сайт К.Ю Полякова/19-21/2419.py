f4b5a31427089553aae49730a8df96b2d5089c71
Marat2004 <testinger2021@yandex.ru>
2022-07-10 08:36:13 +0500

    19-21

---
+++ 2021 и 2022/Сайт К.Ю Полякова/19-21/2419.py
@@ -0,0 +1,12 @@
+def f(a, b, c, m):
+    if a + b >= 62: return c % 2 == m % 2
+    if c == m: return 0
+    h = [f(a + 3, b, c + 1, m), f(a, b + 3, c + 1, m), f(a * 2, b, c + 1, m), f(a, b * 2, c + 1, m)]
+    return any(h) if (c + 1) % 2 == m % 2 else all(h)
+
+
+for b in range(1, 55):
+    for m in range(1, 5):
+        if f(7, b, 0, m) == 1:
+            if m == 4: print(b, m)
+            break
